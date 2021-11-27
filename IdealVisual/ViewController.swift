45e8366c266aac277c27cb2f0e57eaa5077ffa3b
Alexandra Kurganova <kurganova@vbankcenter.ru>
2021-11-27 20:48:35 +0300

    Initial Commit

---
+++ IdealVisual/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IdealVisual
+//
+//  Created by Sasha Kurganova on 27.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
